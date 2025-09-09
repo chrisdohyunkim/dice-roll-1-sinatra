67016b4d6ade2f2ce54374c30d7035f7fc2463c1
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-09 23:34:57 +0000

    Update second erb

--- dice.rb
+++ dice.rb
@@ -9,34 +9,16 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
-end
-
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+  erb(:elephant)
 end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
