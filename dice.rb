7266ba4529d9fa1b9a52e9b02fd756a8e90792a6
Chris Kim <chrisdohyunkim@gmail.com>
2025-09-10 00:17:34 +0000

    Finish styling

--- dice.rb
+++ dice.rb
@@ -13,41 +13,45 @@ get("/") do
 end
 
 get("/dice/2/6/") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    dice = rand(1..6)
+    @rolls.push(dice)
+  end
   
   erb(:two_six)
 end
 
 get("/dice/2/10/") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-  
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    dice = rand(1..10)
+    @rolls.push(dice)
+  end
   
   erb(:two_ten)
 end
 
 get("/dice/1/20/") do
-  die = rand(1..20)
-  
-  @outcome = "You rolled a #{die}."
+  @rolls = []
+
+  1.times do
+    dice = rand(1..20)
+    @rolls.push(dice)
+  end
   
   erb(:one_twenty)
 end
 
 get("/dice/5/4/") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  @rolls = []
+
+  5.times do
+    dice = rand(1..4)
+    @rolls.push(dice)
+  end
   
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-  
-  erb(:five_four, { :layout => :wrapper})
+  erb(:five_four)
 end
